76276b56b7a5155653455c1bacb4455f89fc49b7
RaKDxebic <narak.chhoam@gmail.com>
2026-03-24 20:31:39 +0700

    fix: conflicting issue

--- backend/database/migrations/2026_03_08_100000_create_faculties_table.php
+++ backend/database/migrations/2026_03_08_100000_create_faculties_table.php
@@ -13,10 +13,7 @@ return new class extends Migration {
         Schema::create('faculties', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-<<<<<<<< HEAD:backend/database/migrations/2026_03_01_104427_faculties.php
             $table->string('code')->unique();
-========
->>>>>>>> c5cc328259959343239e08ca02d705f63e91810e:backend/database/migrations/2026_03_08_100000_create_faculties_table.php
             $table->timestamps();
         });
     }
